6b0fb52730877b3f76174951d334760a9d7a9102
stedn <will.stedden@gmail.com>
2020-04-24 01:13:14 -0700

    remove cruft fix readme

--- README.md
+++ README.md
@@ -1,51 +1,22 @@
-# Multiplayer Online TicTacToe
-A Tic tac toe game made with Angular JS and firebase as database.
-Play online with your friends, track your score and compete with them on the leaderboard.
-
-# Table of Contents
-* [Getting Started](#getting-started)
-* [How to Play?](#how-to)
-* [Live Project](#live-project)
-* [Features](#features)
-* [Built with](#built-with)
-* [Author](#author)
- 
-# <a name="getting-started"></a>Getting Started
-First thing you need to do to use this project is to clone it using the following command -
-```
-git clone https://github.com/prateek3255/MultiplayerOnlineTicTacToe.git
-```
-Initially you would need to replace the firebase config in the index.html with your own firebase config settings so that it works on your firebase database, for more details on how to do that you can follow the process described [here](https://firebase.google.com/docs/web/setup).
-
-The project is divided into three sections namely Login, Leaderboard and Game. Each of these pages have their separate html files under the views folder.
-App.js also has three separate controllers for each of these components namely `LoginCtrl`, `LeaderCtrl` and `tic` respectively.
+# Virtual Black Rock City
+A 2D simulation of that annual temporary city in the middle of the Black Rock Desert Nevada.  I'm working on building a place where thousands of people can hang out and find cool stuff to look at for a virtual burning man in 2020.
 
 
 # <a name="how-to"></a>How to Play?
 <ol>
-` <li> Sign in using any of the provided services - Google or Facebook</li>
-  <li> See the currently online users and challenge them using the button provided against their name.</li>
-  <li> If someone else challanges you then you will get a message to accept or reject their request.</li>
-  <li> Once your accept someone else's request or vice-versa then you will be directed towards the game screen.</li>
-  <li> You can play the game using the buttons provided in the grid when it is your turn.</li>
-  <li> Once the game is completed both the players will be directed back to the leaderboard and the winners score will be updated on the leaderboard.</li>
+` <li> Sign in - you can use any email and password combo (password needs to be 8 characters) or you can use google auth</li>
+  <li> Use the arrow keys to move around, put some text in your text box.</li>
+  <li> Get up to some shenagigans.</li>
 </ol>
 
-# <a name="live-project"></a> Live Project
-Multiplayer TicTacToe is live <a href="https://tic-tac-toe-3eed7.firebaseapp.com">here.</a> 
-
-# <a name="features"></a>Features
-* Easy Sign in
-* Track your score on the leaderboard
-* Easy gameplay
-* See all the online users and challenge them
+# <a name="live-project"></a> Demo Server
+you can view the current demo version  <a href="https://v-playa.web.app">here.</a>
 
 # <a name="built-with"></a>Built with
 * <a href="https://angularjs.org/">Angular JS</a> - Superheroic JavaScript MVW Framework
 * <a href="https://firebase.google.com/">Firebase</a> - Used for Authentication, Database, Storage and Hosting in thi app.
-* <a href="http://getbootstrap.com/">Bootstrap</a> - Used in designing.
-* Programming languages used - HTML5, CSS3, JavaScript
 
-# <a name="author"></a>Author
-* <b>Prateek Surana   </b>
-<a href="mailto:prateeksurana3255@gmail.com">Email</a>
+# <a name="author"></a>Get in touch
+If you want to help develop, let me know!
+* <b>Will Stedden  </b>
+<a href="https://twitter.com/bonkerfield">Twitter</a>
--- app.js
+++ app.js
@@ -27,10 +27,12 @@ function loginCtrl($firebaseAuth, $location, $firebaseObject, $scope) {
             leaderboard.$loaded().then(function () {
                 if (leaderboard.lat == null){
                     leaderboard.displayName = result.user.displayName;
-                    leaderboard.photoURL = result.user.photoURL;
+                    // leaderboard.photoURL = result.user.photoURL;
+                    leaderboard.photoURL = "#"+Math.floor(Math.random()*16777215).toString(16);
                     leaderboard.msg = "";
-                    leaderboard.lat = 0;
-                    leaderboard.lon = 0;
+                    leaderboard.lat = -7500 + Math.floor(Math.random()*500);
+                    leaderboard.lon = -250 + Math.floor(Math.random()*500);
+                    leaderboard.dark = "dark";
                 }
                 var gX = Math.floor(leaderboard.lon/gridSizeX);
                 var gY = Math.floor(leaderboard.lat/gridSizeY);
@@ -38,11 +40,12 @@ function loginCtrl($firebaseAuth, $location, $firebaseObject, $scope) {
                 var gridRef = firebase.database().ref("grid/" + leaderboard.gridId +'/'+result.user.uid);
                 grid = $firebaseObject(gridRef);
 
-                grid.displayName = result.user.displayName;
-                grid.photoURL = result.user.photoURL;
-                grid.msg = "";
+                grid.displayName = leaderboard.displayName;
+                grid.photoURL = leaderboard.photoURL;
+                grid.msg = leaderboard.msg;
                 grid.lat = leaderboard.lat;
                 grid.lon = leaderboard.lon;
+                grid.dark = leaderboard.dark;
 
                 grid.$save().then(function () {
                     leaderboard.$save().then(function () {
@@ -53,15 +56,92 @@ function loginCtrl($firebaseAuth, $location, $firebaseObject, $scope) {
 
             });
 
-            // $location.path("/leaderboard");
         })
-            .catch(function (error) {
-                console.error("Authentication failed:", error);
-            });
+        .catch(function (error) {
+            console.error("Authentication failed:", error);
+        });
     }
 
+    login.loginWithEmail = function () {
+        displayname = document.getElementById('displayname').value
+        email = document.getElementById('email').value
+        password = document.getElementById('password').value
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function (result) {
+            var leaderRef = firebase.database().ref("leaderboard/" + result.uid);
+            var leaderboard = $firebaseObject(leaderRef);
+            leaderboard.$loaded().then(function () {
+                if (leaderboard.lat == null){
+                    leaderboard.displayName = displayname;
+                    // leaderboard.photoURL = result.user.photoURL;
+                    leaderboard.photoURL = "#"+Math.floor(Math.random()*16777215).toString(16);
+                    leaderboard.msg = "";
+                    leaderboard.lat = -7500 + Math.floor(Math.random()*500);
+                    leaderboard.lon = -250 + Math.floor(Math.random()*500);
+                    leaderboard.dark = "dark";
+                }
+                var gX = Math.floor(leaderboard.lon/gridSizeX);
+                var gY = Math.floor(leaderboard.lat/gridSizeY);
+                leaderboard.gridId = gX + '_' + gY
+                var gridRef = firebase.database().ref("grid/" + leaderboard.gridId +'/'+result.uid);
+                grid = $firebaseObject(gridRef);
 
+                grid.displayName = leaderboard.displayName;
+                grid.photoURL = leaderboard.photoURL;
+                grid.msg = leaderboard.msg;
+                grid.lat = leaderboard.lat;
+                grid.lon = leaderboard.lon;
+                grid.dark = leaderboard.dark;
 
+                grid.$save().then(function () {
+                    leaderboard.$save().then(function () {
+                        $location.path("/game");
+                        console.log("working just fine");
+                    });
+                });
+
+            });
+        })
+        .catch(function(error) {
+            firebase.auth().signInWithEmailAndPassword(email, password).then(function (result) {
+                var leaderRef = firebase.database().ref("leaderboard/" + result.uid);
+                var leaderboard = $firebaseObject(leaderRef);
+                leaderboard.$loaded().then(function () {
+                    if (leaderboard.lat == null){
+                        leaderboard.displayName = displayname;
+                        // leaderboard.photoURL = result.user.photoURL;
+                        leaderboard.photoURL = "#"+Math.floor(Math.random()*16777215).toString(16);
+                        leaderboard.msg = "";
+                        leaderboard.lat = -7500 + Math.floor(Math.random()*500);
+                        leaderboard.lon = -250 + Math.floor(Math.random()*500);
+                        leaderboard.dark = "dark";
+                    }
+                    var gX = Math.floor(leaderboard.lon/gridSizeX);
+                    var gY = Math.floor(leaderboard.lat/gridSizeY);
+                    leaderboard.gridId = gX + '_' + gY
+                    var gridRef = firebase.database().ref("grid/" + leaderboard.gridId +'/'+result.uid);
+                    grid = $firebaseObject(gridRef);
+
+                    grid.displayName = leaderboard.displayName;
+                    grid.photoURL = leaderboard.photoURL;
+                    grid.msg = leaderboard.msg;
+                    grid.lat = leaderboard.lat;
+                    grid.lon = leaderboard.lon;
+                    grid.dark = leaderboard.dark;
+
+                    grid.$save().then(function () {
+                        leaderboard.$save().then(function () {
+                            $location.path("/game");
+                            console.log("working just fine");
+                        });
+                    });
+
+                });
+            })
+            .catch(function(error) {
+                console.log(error)
+            });
+        });
+    }
 }
 
 
@@ -88,11 +168,15 @@ function tic($routeParams, $firebaseObject, $firebaseAuth,$location,$firebaseArr
 
     gameloop = setInterval(update, 100);
     peeppoll = setInterval(pollPeeps, 1000);
-    clientTime = 0;
-    longupdate = setInterval(longtemps, 10000);
+
+    sceneUpdateTimeSeconds = 0.1*60.0
+
+    longtemps();
+    setInterval(longtemps, sceneUpdateTimeSeconds*1000);
     document.addEventListener("keydown",keyDownHandler, false);
     document.addEventListener("keyup",keyUpHandler, false);
     t.neighbors = []
+    t.doubleCheck = []
 
     function keyDownHandler(event) {
         if (event.keyCode == '38') {
@@ -161,7 +245,7 @@ function tic($routeParams, $firebaseObject, $firebaseAuth,$location,$firebaseArr
         for (var i = 0; i < t.neighbors.length; i++) {
             n = t.neighbors[i]
             elem = document.createElement("h5");
-            elem.className = "neighbors"
+            elem.className = "neighbors "+n.dark
             elem.style.textAlign = "center"
             elem.style.position = "fixed"
             elem.style.top = (-n.lat + t.leaderboard.lat) + (y - icon_size/2) + "px"
@@ -169,40 +253,91 @@ function tic($routeParams, $firebaseObject, $firebaseAuth,$location,$firebaseArr
             elem.innerHTML = n['innerHTML']
             document.body.appendChild(elem);
         }
-
         svgElement = document.getElementById('brcmap')
         svgElement.style.left = (-(svgElement.width/2)+t.leaderboard.lon+x)+'px';
         svgElement.style.top = (-(svgElement.height/2)+t.leaderboard.lat+y)+'px';
     }
 
     function pollPeeps() {
+        led_check = document.getElementById('leds');
+        if (led_check!=null){
+            if (!t.night){
+                led_check.checked = false;
+            }
+            leds = led_check.checked;
+            if (leds && t.leaderboard.dark=='dark'){
+                t.leaderboard.dark = 'led';
+                t.grid.dark = 'led';
+                t.leaderboard.$save();
+                t.grid.$save();
+            } else if (!leds && t.leaderboard.dark=='led'){
+                t.leaderboard.dark = 'dark';
+                t.grid.dark = 'dark';
+                t.leaderboard.$save();
+                t.grid.$save();
+            }
+        } else{
+            t.leaderboard.dark = 'dark';
+            t.grid.dark = 'dark';
+            t.leaderboard.$save();
+            t.grid.$save();
+        }
+
+
+
         var gX = Math.floor(t.leaderboard.lon/gridSizeX);
         var gY = Math.floor(t.leaderboard.lat/gridSizeY);
         var gridId = gX + '_' + gY
         var msg = document.getElementById('message').value
+
+        // update grid location
         if (t.leaderboard.gridId != gridId){
-            firebase.database().ref("grid/" + t.leaderboard.gridId +'/'+t.user.uid).remove().then(function (){
-                t.leaderboard.gridId = gridId;
+            firebase.database().ref("grid/" + t.leaderboard.gridId +'/'+t.user.uid).remove()
+            t.doubleCheck.push(t.leaderboard.gridId)
 
-                var gridRef = firebase.database().ref("grid/" + gridId +'/'+t.user.uid);
-                t.grid = $firebaseObject(gridRef);
+            t.leaderboard.gridId = gridId;
+            t.leaderboard.$save();
 
-                t.grid.displayName = t.user.displayName;
-                t.grid.photoURL = t.user.photoURL;
-                t.grid.msg = msg;
-                t.grid.lat = t.leaderboard.lat;
-                t.grid.lon = t.leaderboard.lon;
-                t.grid.$save();
+            var gridRef = firebase.database().ref("grid/" + gridId +'/'+t.user.uid);
+            t.grid = $firebaseObject(gridRef);
 
-            });
+            t.grid.displayName = t.leaderboard.displayName;
+            t.grid.photoURL = t.leaderboard.photoURL;
+            t.grid.msg = msg;
+            t.grid.$save();
         }
 
+        // double check that everything has been removed
+        newDoubleCheck = []
+        for (i=0; i < t.doubleCheck.length; i++){
+            if (t.doubleCheck[i]!=t.leaderboard.gridId){
+                var checkRef = firebase.database().ref("grid/" + t.doubleCheck[i] +'/'+t.user.uid);
+                checkRef.on('value', function(snapshot) {
+                   if (snapshot.exists()){
+                      firebase.database().ref("grid/" + t.doubleCheck[i] +'/'+t.user.uid).remove();
+                      newDoubleCheck.push(t.doubleCheck[i]);
+                   }
+                });
+            }
+        }
+        t.doubleCheck = newDoubleCheck
+
+        // update message
         if (msg != t.leaderboard.msg){
             t.leaderboard.msg = msg;
             t.leaderboard.$save();
             t.grid.msg = msg;
-            t.grid.$save();
         }
+        if (!t.night){
+            t.dark = false;
+        }
+
+        t.grid.lat = t.leaderboard.lat;
+        t.grid.lon = t.leaderboard.lon;
+        t.grid.dark = t.leaderboard.dark;
+        t.grid.$save();
+
+        // find neighbors and add them to display list
         t.neighbors = [];
         var gX = Math.floor(t.leaderboard.lon/gridSizeX);
         var gY = Math.floor(t.leaderboard.lat/gridSizeY);
@@ -215,7 +350,14 @@ function tic($routeParams, $firebaseObject, $firebaseAuth,$location,$firebaseArr
                             displayObj = {};
                             displayObj.lat = childNode.val().lat;
                             displayObj.lon = childNode.val().lon;
-                            displayObj.innerHTML = childNode.val().displayName+'<br/><img src="'+childNode.val().photoURL+'" alt="loading" class="img-circle image"><br/><span style="border-color: grey; border-style: solid; border-width:thin;padding:0 2px;width:20px;">'+childNode.val().msg+'</span>';
+                            displayObj.dark = childNode.val().dark;
+                            // photo = '<img src="'+childNode.val().photoURL+'" alt="loading" class="img-circle image">'
+                            circleColor = childNode.val().photoURL;
+                            if (t.night && childNode.val().dark=='dark'){
+                                circleColor = 'black';
+                            }
+                            photo = '<svg height="100" width="100"><circle cx="50" cy="50" r="40" stroke="'+circleColor+'" stroke-width="3" fill="'+circleColor+'" /></svg>'
+                            displayObj.innerHTML = childNode.val().displayName+'<br/>'+photo+'<br/><span style="border-color: black; border-style: solid; border-width:thin;padding:0 2px;width:20px;">'+childNode.val().msg+'</span>';
                             t.neighbors.push(displayObj);
                         }
                     });
@@ -224,21 +366,65 @@ function tic($routeParams, $firebaseObject, $firebaseAuth,$location,$firebaseArr
         }
     }
 
+    // runs periodically
     function longtemps() {
-        // var clientdate = new Date();
-        // var clientTime = clientdate.getHours();
-        clientTime = clientTime + 1;
-        var r0 = 252;
-        var g0 = 249;
-        var b0 = 242;
-        var r1 = 237;
-        var g1 = 147;
-        var b1 = 82;
-        var rt = r0 + (r1-r0)*(clientTime/23.0);
-        var gt = g0 + (g1-g0)*(clientTime/23.0);
-        var bt = b0 + (b1-b0)*(clientTime/23.0);
+        var envRef = firebase.database().ref("environment");
+        var environment = $firebaseObject(envRef);
+        environment.$loaded().then(function () {
+            document.getElementById('whiteout').style.opacity = environment.opacity;
+            environment.hour = environment.hour + 0.1;
+            if (environment.hour > 24.0) {
+                environment.hour = 0;
+            }
+            environment.$save();
+            brcHour = environment.hour;
+            // day
+            var r0 = 252;
+            var g0 = 249;
+            var b0 = 242;
+            // sunset
+            var r1 = 237;
+            var g1 = 147;
+            var b1 = 82;
+            // night
+            var r2 = 10;
+            var g2 = 0;
+            var b2 = 0;
 
-
-        document.body.style.backgroundColor = 'rgb(' + rt + ',' + gt + ',' + bt + ')';
+            if (brcHour < 4){
+                var rt = r2;
+                var gt = g2;
+                var bt = b2;
+                t.night = true;
+            } else if (brcHour < 6) {
+                var rt = r2 + (r0-r2)*(brcHour - 4)/2.0;
+                var gt = g2 + (g0-g2)*(brcHour - 4)/2.0;
+                var bt = b2 + (b0-b2)*(brcHour - 4)/2.0;
+                t.night = false;
+            } else if (brcHour < 18){
+                var rt = r0;
+                var gt = g0;
+                var bt = b0;
+                t.night = false;
+            } else if (brcHour < 19) {
+                var rt = r0 + (r1-r0)*(brcHour-18);
+                var gt = g0 + (g1-g0)*(brcHour-18);
+                var bt = b0 + (b1-b0)*(brcHour-18);
+                t.night = false;
+            } else if (brcHour < 20) {
+                var rt = r1 + (r2-r1)*(brcHour-19);
+                var gt = g1 + (g2-g1)*(brcHour-19);
+                var bt = b1 + (b2-b1)*(brcHour-19);
+                t.night = true;
+            } else {
+                var rt = r2;
+                var gt = g2;
+                var bt = b2;
+                t.night = true;
+            }
+            document.body.style.backgroundColor = 'rgb(' + rt + ',' + gt + ',' + bt + ')';
+        });
     }
 }
+
+
